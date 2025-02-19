f7f6257d43eba8bfc76a8a6cd4a4c10008382316
darshitbhalodi5 <darshitbhalodi@gmail.com>
2025-02-19 15:48:40 +0530

    Removed statastic page and component as not necessary

--- app/stats/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BountyStats } from '@/components/dashboard/BountyStats';
-
-export default function StatsPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <BountyStats />
-    </div>
-  );
-} 
